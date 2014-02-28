7152e01dc16e28df57de692c70c9c38f0545a22f
danielrgreen <drgreen@fas.harvard.edu>
2014-02-27 21:38:05 -0500

    blood to toothmap now does zscore comparisons

--- mineralization/small_blood_to_toothmap.py
+++ mineralization/small_blood_to_toothmap.py
@@ -27,6 +27,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 from bloodhist import calc_blood_hist
+from PIL import Image
+#from scipy.misc import imresize
 
 def d18O_pixel(age, increase, blood_hist, x, y):
 
@@ -58,6 +60,35 @@ def smoothListGaussian(list,degree=20):
      for i in range(len(smoothed)):  
          smoothed[i]=sum(np.array(list[i:i+window])*weight)/sum(weight)
      return smoothed
+
+def imresize(x, shape, method=Image.BILINEAR):
+    assert len(x.shape) == 2
+    
+    im = Image.fromarray(x)
+    im_resized = im.resize(shape, method)
+    
+    x_resized = np.array(im_resized.getdata()).reshape(shape[::-1]).T
+    
+    return x_resized
+
+def z_calc(x_resized):
+
+    data = np.array([0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 11.58, 11.39, 13.26, 12.50, 11.88, 9.63, 13.46, 12.83, 11.60, 12.15, 11.57, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 11.74, 12.29, 12.38, 12.31, 12.24, 12.26, 11.79, 12.07, 13.13, 13.37, 12.41, 13.31, 13.77, 13.51, 13.53, 13.41, 13.57, 13.99, 13.61, 13.43, 13.40, 12.40, 12.94, 12.43, 12.10, 11.13, 10.90, 10.17, 10.06, 0.00, 0.00, 0.00, 12.17, 11.95, 11.00, 11.92, 12.55, 12.29, 12.39, 12.08, 12.91, 10.38, 12.70, 12.69, 12.23, 12.56, 11.53, 12.82, 12.36, 12.51, 10.69, 11.33, 13.33, 13.12, 13.21, 13.07, 13.76, 16.53, 14.63, 11.81, 9.76, 9.95, 10.07, 10.42, 0.00, 0.00, 12.40, 12.17, 12.21, 11.04, 12.81, 12.20, 12.69, 12.31, 12.44, 13.11, 10.84, 12.85, 12.90, 13.13, 13.74, 13.18, 11.91, 12.53, 13.10, 12.28, 12.92, 10.95, 12.83, 13.20, 13.25, 12.10, 11.95, 12.08, 11.65, 8.45, 10.18, 11.02, 0.00, 13.01, 12.39, 12.05, 12.25, 13.42, 12.68, 11.84, 12.43, 10.19, 11.24, 10.55, 11.33, 12.09, 12.56, 13.71, 12.03, 10.78, 12.75, 12.67, 12.50, 12.48, 12.50, 11.96, 12.21, 12.28, 9.88, 11.85, 12.44, 11.07, 11.18, 10.68, 11.42, 12.39, 10.08])
+    for i in data:
+        if i == 0:
+            i = np.nan
+            
+    data = np.reshape(data, (5,34))
+    
+    compare = np.absolute(data - x_resized)
+    idx = np.isnan(compare)
+    compare[idx] = 0.
+    
+    compare2 = np.square(compare)
+    z_raw = np.sum(compare2)
+    z = z_raw**.5
+    
+    return (data, compare, z)
 
 def main():
 
@@ -100,7 +131,18 @@ def main():
 
     d18O_map = np.delete(d18O_map, np.s_[185:-1], 0)
 
+    shape = (34, 6)
     
+    x_resized = imresize(d18O_map, shape, method=Image.BILINEAR)
+    #testmap = imresize(d18O_map, new_dimension, interp='bilinear', mode=none)
+    x_resized = x_resized.T
+    x_resized = np.delete(x_resized, np.s_[0:1], 0)
+
+    data, compare, z = z_calc(x_resized)
+
+    print 'data', data
+    print 'compare', compare
+    print 'z', z
     
     d18O_total, daily_increase, d18O_addition = d18O_pixel(age, increase, blood_hist, 80, 15)
 
@@ -118,13 +160,14 @@ def main():
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     
-    cimg = ax.imshow(d18O_map, origin='lower', aspect='auto', interpolation='none')
+    cimg = ax.imshow(x_resized, origin='lower', aspect='auto', interpolation='none')
     cax = fig.colorbar(cimg)
     
     plt.show()
     
     
     return 0
+
 
 if __name__ == '__main__':
     main()
