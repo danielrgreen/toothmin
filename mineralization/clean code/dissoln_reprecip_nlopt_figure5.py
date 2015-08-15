da15a9a202441519765cfe82bab8c5951a86d3c0
danielrgreen <drgreen@fas.harvard.edu>
2015-08-15 16:57:16 +0200

    fixing one figure file for fig 5

--- mineralization/clean code/dissoln_reprecip_nlopt_figure5.py
+++ mineralization/clean code/dissoln_reprecip_nlopt_figure5.py
@@ -658,7 +658,7 @@ def water_hist_prob_4param(w_params, **kwargs):
     prior = prior_score(w_params)
     #p += prior
 
-    list_params = np.array([w_params[0], w_params[1], w_params[2], w_params[3], w_params[4], w_params[5], w_params[6]])
+    list_params = np.array([w_params[0], w_params[1], w_params[2], w_params[3], PO4_t, PO4_pause, PO4_flux])
     list_tuple = (p, list_params)
     my_list.append(list_tuple)
 
@@ -881,7 +881,9 @@ def fit_tooth_data(data_fname, model_fname='equalsize_jul2015a.h5', **kwargs):
     for s in save_list:
         s_params = s[1]
         s_times = s_params[2:4]
-        s_PO4 = s_params[4:6]
+        print s_times.size
+        s_PO4 = s_params[4:7]
+        print s_PO4.size
         s_times[1] = s_times[0] + s_times[1]
         s_times = tooth_timing_convert(s_times, *m1_m2_params)
         s_times[1] = s_times[1] - s_times[0]
@@ -943,19 +945,22 @@ def fit_tooth_data(data_fname, model_fname='equalsize_jul2015a.h5', **kwargs):
     blood_hist = blood_delta(23.5, w_iso_hist, 25.3, **kwargs)
     phosphate_eq = PO4_dissoln_reprecip(PO4_t, PO4_pause, PO4_flux, blood_hist, **kwargs)
     days = np.arange(blood_hist.size)
-    ax1.plot(days, real_switch_hist, 'k-.', linewidth=1.0)
-    ax1.plot(days, w_iso_hist, 'b-', linewidth=2.0)
-    ax1.plot(days, blood_hist, 'r-', linewidth=2.0)
-    ax1.plot(days, phosphate_eq, 'g-.', linewidth=1.0)
-    ax1.plot(blood_days, blood_measures, 'r*', linewidth=1.0)
-    ax1.plot(water_iso_days, water_iso_measures, 'b*', linewidth=1.0)
-    for s in list_water_results:
-        ax1.plot(days, s, 'b-', alpha=0.05)
+    #ax1.plot(days, real_switch_hist, 'k-.', linewidth=1.0)
+    ax1.plot(days, w_iso_hist, 'b-', linewidth=2.0, label=r'$\mathrm{water} \ \delta^{18}\!\mathrm{O} \ \mathrm{(modeled)}$', alpha=1)
+    ax1.plot(days, blood_hist, 'r-', linewidth=2.0, label=r'$\mathrm{blood} \ \delta^{18}\!\mathrm{O} \ \mathrm{(modeled)}$', alpha=1)
+    ax1.plot(blood_days, blood_measures, 'r*', linewidth=1.0, label=r'$\mathrm{blood} \ \delta^{18}\!\mathrm{O} \ \mathrm{(measured)} $', alpha=1)
+    ax1.plot(water_iso_days, water_iso_measures, 'b*', linewidth=1.0, label=r'$\mathrm{water} \ \delta^{18}\!\mathrm{O} \ \mathrm{(measured)}$', alpha=1)
+    for s in list_water_results[:-1]:
+        ax1.plot(days, s, 'g-', alpha=0.1)
+    ax1.plot(days, list_water_results[-1], 'g-', label=r'$ \mathrm{PO}_4^{eq} \ \delta^{18}\!\mathrm{O} \ \mathrm{(trials)}$', alpha=.1)
+    h1, l1 = ax1.get_legend_handles_labels()
+    ax1.plot(days, phosphate_eq, 'g-.', linewidth=2.0, label=r'$ \mathrm{PO}_4^{eq} \ \delta^{18}\!\mathrm{O} \ \mathrm{(optimum)}$', alpha=1)
+    ax1.legend(h1, l1, loc='bottom left', fontsize=10)
     #vmin = np.min(np.concatenate((real_switch_hist, w_iso_hist, blood_hist), axis=0)) - 1.
     #vmax = np.max(np.concatenate((real_switch_hist, w_iso_hist, blood_hist), axis=0)) + 1.
     #ax1.text(350, -15, textstr, fontsize=8)
     ax1.set_ylim(-30, 0)
-    ax1.set_xlim(-100, 550)
+    ax1.set_xlim(50, 350)
 
     #fig.savefig('a-synch86o_forward_PO4p35_newdelta_{0}b.svg'.format(t_save), dpi=300, bbox_inches='tight')
     plt.show()
